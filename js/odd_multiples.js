a5d75c9c9cc6b8aeb44b4e5e7e1a3a73a08ff1f8
Micah-N <mnetz@2ndwatch.com>
2017-06-27 10:59:12 -0700

    Cleaned up file tree

---
+++ js/odd_multiples.js
@@ -0,0 +1,12 @@
+//Micah Netz
+
+var result = 0;
+
+for(var i = 0; i < 1000; i++){
+	if(i%3 === 0 || i%5 === 0){
+		result += i;
+	}
+}
+
+console.log("The sum result of all multiples of 3 or 5 from 0 to 1000 is: " + result);
+document.getElementById('odd_multiples').innerHTML = result;
